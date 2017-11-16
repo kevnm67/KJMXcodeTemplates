68c280e54899419702aef2a409cd6b7ae88c0e34
Kevin Morton <kmorton@roadie.com>
2017-11-16 11:15:48 -0500

    Added basic unit test class with icon

---
+++ Templates/File Templates/Custom/Unit Test Case Class.xctemplate/XCTestCaseObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,26 @@
+//___FILEHEADER___
+
+#import <XCTest/XCTest.h>
+
+#import <Expecta/Expecta.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_testSubclass___
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Lifecycle -
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    
+    [super tearDown];
+}
+
+#pragma mark - Tests -
+
+@end
